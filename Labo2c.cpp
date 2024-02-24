7a74a3802bb20ad52c857a66c47dfae8b639981c
mathieuLeGouill@outlook.com <mathieuLeGouill@outlook.com>
2024-02-24 12:39:12 -0500

    correction et ajout des criteres taboux

--- Labo2c.cpp
+++ Labo2c.cpp
@@ -44,13 +44,13 @@ TSolution GetSolutionVoisine (const TSolution uneSol, TProblem unProb, TAlgo &un
 
 //DESCRIPTION:	Application du type de voisinage selectionne. La fonction retourne la solution voisine obtenue suite a l'application du type de voisinage.
 //NB:uneSol ne doit pas etre modifiee (const)
-TSolution AppliquerVoisinage(const TSolution uneSol, TProblem unProb, TAlgo& unAlgo);
+TSolution AppliquerVoisinage(const TSolution uneSol, TProblem unProb, TAlgo& unAlgo, vector<int>& bloc);
 
 
-bool SolEstAdmissible(const vector<TSolution>& listeTaboux, const TSolution& sol, long critere_aspiration);
+bool SolEstAdmissible(const vector<vector<int>>& listeTaboux, const TSolution& sol, long critere_aspiration, const vector<int>& bloc);
 
 //DESCRIPTION: Deplacement de 1-2-3 villes selectionnees aleatoirement.												
-void OrOPT(TSolution& Voisin, TProblem unProb, TAlgo& unAlgo);
+void OrOPT(TSolution& Voisin, TProblem unProb, TAlgo& unAlgo, vector<int>& bloc);
 
 //******************************************************************************************
 // Fonction main
@@ -114,59 +114,66 @@ TSolution GetSolutionVoisine (const TSolution uneSol, TProblem unProb, TAlgo &un
 	//Parcours dans le voisinage : 		Aleatoire: taille du bloc aleatoire entre [1,3], positions de selection et d'insertion aleatoires 
 	//Regle de pivot : 					k-ImproveBEST (k étant donné en paramètre pour l'exécution du pgm)
 
-	std::vector<TSolution> listeTaboux(unAlgo.LngListeTabous, TSolution());
+	static std::vector<std::vector<int>> listeTaboux(unAlgo.LngListeTabous, std::vector<int>{});
+	static int idTabou = 0;
 
 	//k-Improve-Best
 	TSolution unVoisin, unGagnant;
 	int i;
+
+	// Initialisation du bloc de villes voisines
+	std::vector<int> blocVoisin;
 	
-	unGagnant = AppliquerVoisinage(uneSol, unProb, unAlgo); //Premier voisin
+
+	// On accepte le premier voisin comme solution courante
+	unGagnant = AppliquerVoisinage(uneSol, unProb, unAlgo, blocVoisin); //Premier voisin
+
 	static long critere_aspiration = unGagnant.FctObj;
+	listeTaboux.push_back(blocVoisin);
 
 	for (i = 1; i < unAlgo.TailleVoisinage; i++) //Permet de generer plusieurs solutions voisines
 	{
-		unVoisin = AppliquerVoisinage(uneSol, unProb, unAlgo);
+		unVoisin = AppliquerVoisinage(uneSol, unProb, unAlgo, blocVoisin);
 
 		// Si le voisin est admissible 
-		if(SolEstAdmissible(listeTaboux, unVoisin, critere_aspiration))
+		if(SolEstAdmissible(listeTaboux, unVoisin, critere_aspiration, blocVoisin))
 		{
 			if (unVoisin.FctObj < unGagnant.FctObj) //Conservation du meilleur
+			{
 				unGagnant = unVoisin;
+
+				// Mise à jour du critère d'aspiration
+				critere_aspiration = unGagnant.FctObj;
+
+				// Ajout du bloc à la liste des taboux
+				listeTaboux[idTabou % unAlgo.LngListeTabous] = blocVoisin;
+
+				idTabou++;
+			}
+				
 			else //Choix aléatoire si unVoisin et un Gagnant sont de même qualité
 				if (unVoisin.FctObj == unGagnant.FctObj)
 				{
 					if (rand() % 2 == 0)
+					{
 						unGagnant = unVoisin;
+
+						// Ajout du bloc à la liste des taboux
+						listeTaboux[idTabou % unAlgo.LngListeTabous] = blocVoisin;
+
+						idTabou++;
+					}
 				}
 		}
 	}
 	return (unGagnant);
 }
 
-//DESCRIPTION: Fonction appliquant le type de voisinage selectionne. La fonction retourne la solution voisine obtenue suite a l'application du type de voisinage.
-//NB: La solution courante (uneSol) ne doit pas etre modifiee (const)
-TSolution AppliquerVoisinage(const TSolution uneSol, TProblem unProb, TAlgo& unAlgo)
-{
-	//Type (structure) de voisinage : 	OrOPT - Deplacement de 1, 2 ou 3 villes selectionnees aleatoirement	
-	TSolution Copie;
 
-	//Utilisation d'une nouvelle TSolution pour ne pas modifier La solution courante (uneSol)
-	Copie = uneSol;
-
-	//Transformation de la solution Copie selon le type (structure) de voisinage selectionne : OrOPT
-	OrOPT(Copie, unProb, unAlgo);																										
-
-	//Le nouveau voisin doit etre evalue et retourne
-	EvaluerSolution(Copie, unProb, unAlgo);
-	return(Copie);
-}
-
-
-bool SolEstAdmissible(const vector<TSolution>& listeTaboux, const TSolution& sol, long critere_aspiration)
+bool SolEstAdmissible(const vector<vector<int>>& listeTaboux, const TSolution& sol, long critere_aspiration, const vector<int>& bloc)
 {
 	// Si la solution n'est pas présente dans la liste des taboux ou qu'elle vérifie le critère d'aspiration
-	if (std::find(listeTaboux.begin(), listeTaboux.end(), sol) == listeTaboux.end() 
-		|| sol.FctObj < critere_aspiration)
+	if (std::find(listeTaboux.begin(), listeTaboux.end(), bloc) == listeTaboux.end() || sol.FctObj < critere_aspiration)
 	{
 		// La solution est admissible
 		return true;
@@ -175,14 +182,36 @@ bool SolEstAdmissible(const vector<TSolution>& listeTaboux, const TSolution& sol
 	return false;
 }
 
-//DESCRIPTION: Deplacement de 1-2-3 villes selectionnees aleatoirement
-void OrOPT(TSolution& Voisin, TProblem unProb, TAlgo& unAlgo)
+
+//DESCRIPTION: Fonction appliquant le type de voisinage selectionne. La fonction retourne la solution voisine obtenue suite a l'application du type de voisinage.
+//NB: La solution courante (uneSol) ne doit pas etre modifiee (const)
+TSolution AppliquerVoisinage(const TSolution uneSol, TProblem unProb, TAlgo& unAlgo, vector<int>& bloc)
 {
+	//Type (structure) de voisinage : 	OrOPT - Deplacement de 1, 2 ou 3 villes selectionnees aleatoirement	
+	TSolution Copie;
+
+	//Utilisation d'une nouvelle TSolution pour ne pas modifier La solution courante (uneSol)
+	Copie = uneSol;
+
+	//Transformation de la solution Copie selon le type (structure) de voisinage selectionne : OrOPT
+	OrOPT(Copie, unProb, unAlgo, bloc);
+
+	//Le nouveau voisin doit etre evalue et retourne
+	EvaluerSolution(Copie, unProb, unAlgo);
+	return(Copie);
+}
+
+
+//DESCRIPTION: Deplacement de 1-2-3 villes selectionnees aleatoirement
+void OrOPT(TSolution& Voisin, TProblem unProb, TAlgo& unAlgo, vector<int>& bloc)
+{
+	// Remise a zero du bloc
+	bloc.clear();
+
 	#define MAX 3	//Taille maximale pour le bloc de villes a deplacer
 	int PosSelec;	//Variable pour la position du bloc a selectionner
 	int PosInsert;	//Variable pour la position d'insertion
 	int NbVillesDispo, TailleBloc;
-	vector<int> Bloc;
 
 	//Tirage aleatoire de la position de selection du bloc
 	PosSelec = rand() % unProb.NbVilles;
@@ -193,12 +222,12 @@ void OrOPT(TSolution& Voisin, TProblem unProb, TAlgo& unAlgo)
 	TailleBloc = min(TailleBloc, NbVillesDispo);
 
 	//Extraction du bloc a deplacer
-	Bloc = { Voisin.Seq.begin() + PosSelec, Voisin.Seq.begin() + PosSelec + TailleBloc };
+	bloc = { Voisin.Seq.begin() + PosSelec, Voisin.Seq.begin() + PosSelec + TailleBloc };
 	Voisin.Seq.erase(Voisin.Seq.begin() + PosSelec, Voisin.Seq.begin() + PosSelec + TailleBloc);
 
 	//Tirage aleatoire de la position d'insertion (en fonction de la taille restante du vecteur)
 	PosInsert = rand() % Voisin.Seq.size();
 
 	//Insertion du bloc a la position d'insertion
-	Voisin.Seq.insert(Voisin.Seq.begin() + PosInsert, Bloc.begin(), Bloc.end());
+	Voisin.Seq.insert(Voisin.Seq.begin() + PosInsert, bloc.begin(), bloc.end());
 }
